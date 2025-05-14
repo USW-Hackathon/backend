e8a3deb68b8b4829a3717136593d873902e0fb4f
moaguide <moaguide.develop@gmail.com>
2025-05-14 20:22:06 +0900

    Update SecurityConfig.java

--- src/main/java/com/ict/Hackathon/config/SecurityConfig.java
+++ src/main/java/com/ict/Hackathon/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
 	    
-	    configuration.setAllowedOrigins(Arrays.asList("*)); 
+	    configuration.setAllowedOrigins(Arrays.asList("*")); 
 	    
 	    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(Arrays.asList("Authorization", "verify", "Content-Type", "cookie"));
